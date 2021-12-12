0a6247c8209f402052ec8d6701f66724799ceb9b
afranchi2018 <afranchiforever@gmail.com>
2021-12-11 22:12:43 -0500

    ejemplo conexion http
    se crea ejemplo de conexion de R por http p https

--- Consulta _sqlite.R
+++ Consulta _sqlite.R
@@ -1,10 +1,19 @@
+ 
+
 #Si se quiere utilizar postres seria library(RPostgreSQL)
 library(RSQLite)
+library(rs)
 library(DBI)
+library(rgdal)
+
+utils::download.file(file.path('http://x/db.sqlite'),
+                     sqlFile <- tempfile("tp",fileext=".sqlite"),method="libcurl", mode = "wb" )
+print(sqlFile) 
 # Definimos la conexion
-objConecDB <- dbConnect(SQLite(),dbname='db.sqlite')
+objConecDB <- dbConnect(SQLite(),dbname=sqlFile, synchronous= NULL )
 #Esto lista todas las tablas
-#dbListTables(objDB) 
-dataPrios <- dbGetQuery(objConecDB,"SELECT * FROM PrincipalesRiosPanama")
-dbDisconnect(objConecDB)
-print(dataPrios) 
+dbListTables(objConecDB) 
+#dataPrios <- dbGetQuery(objConecDB,"SELECT * FROM PrincipalesRiosPanama")
+
+print(objConecDB) 
+
